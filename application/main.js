daf217ac2c5a8850086e9f890fb366e355c3d1b0
Victor Bastos <victorwpbastos@gmail.com>
2016-10-12 02:23:50 -0300

    initial files and folders

---
+++ application/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import routes from 'routes';
+import Layout from 'views/commons/layout';
+import initializer from 'initializer';
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+let router = new VueRouter({ routes });
+
+initializer();
+
+new Vue({
+	router,
+	render(h) {
+		return h(Layout);
+	}
+}).$mount('#application-container');
